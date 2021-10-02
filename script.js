256a6dbd3cbceb47a8f8298b190d62484a66dbf0
Alex Losev <cahektpx1990@gmail.com>
2021-10-02 12:03:03 +0300

    lesson02 done

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Project Calc";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 500;
+let rollback = 50;
+let fullPrice = 2000;
+let adaptive = true;
 
-alert('Hello world');
+console.log(typeof title, typeof fullPrice, typeof adaptive);
 
-console.log("Hello again");
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов" + " - " + screenPrice * 70 + " Рублей/ ");
+console.log("Стоимость верстки экранов" + " - " + screenPrice + " долларов");
+console.log("Стоимость верстки экранов" + " - " + screenPrice * 26 + " гривен");
+
+console.log("Стоимость разработки сайта" + " - " + fullPrice * 70 + " " + "Рублей");
+console.log("Стоимость разработки сайта" + " - " + fullPrice + " " + "долларов");
+console.log("Стоимость разработки сайта" + " - " + fullPrice * 26 + " " + "гривен");
+console.log("Стоимость разработки сайта" + " - " + fullPrice * 6 + " " + "юаней");
+
+
+screens = screens.toLowerCase();
+console.log(screens.split(", "));
+
+console.log("Процент отката посреднику за работу" + " - " + fullPrice * (rollback / 100));
